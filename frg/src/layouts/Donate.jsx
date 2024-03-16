c24b6a8b12bdbe78c6dba3de22bef746618fdfba
swostikpati <swostikpati@gmail.com>
2024-03-16 13:32:13 +0000

    added donation page

--- frg/src/layouts/Donate.jsx
+++ frg/src/layouts/Donate.jsx
@@ -1,5 +1,11 @@
 import React from "react";
+import "./Donate.css"; // Make sure to create a corresponding CSS file
+import under_construction from "../assets/under-construction.png";
 
 export default function Donate() {
-  return <div>Donate</div>;
+  return (
+    <div className="donate-container">
+      <img src={under_construction} alt="Under Construction" />
+    </div>
+  );
 }
