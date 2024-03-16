c33454c8b05d4239d9606fcf0f7ab45ae1c2d53d
swostikpati <swostikpati@gmail.com>
2024-03-16 17:08:42 +0000

    adding blog page

--- frg/src/layouts/Blog.jsx
+++ frg/src/layouts/Blog.jsx
@@ -1,5 +1,11 @@
 import React from "react";
+import blog from "../assets/blog.png";
+import "./Blog.css";
 
 export default function Blog() {
-  return <div>Blog</div>;
+  return (
+    <div className="blog">
+      <img src={blog} alt="Under Construction" />
+    </div>
+  );
 }
