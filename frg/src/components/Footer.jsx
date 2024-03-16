a83ac5a3aaed913d6111167607f88527b44c660b
swostikpati <swostikpati@gmail.com>
2024-03-16 12:37:24 +0000

    added footer

---
+++ frg/src/components/Footer.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import "./Footer.css"; // Path to your CSS file for styling the footer
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <div className="footer-content">
+        <div className="footer-section">
+          <h4>Contact Us</h4>
+          <p>123 Street Name, London, UK</p>
+          <p>Email: contact@example.com</p>
+          <p>Phone: (123) 456-7890</p>
+        </div>
+        <div className="footer-section">
+          <h4>Follow Us</h4>
+          <p>
+            <a href="https://www.facebook.com">Facebook</a>
+          </p>
+          <p>
+            <a href="https://www.twitter.com">Twitter</a>
+          </p>
+          <p>
+            <a href="https://www.instagram.com">Instagram</a>
+          </p>
+        </div>
+        <div className="footer-section">
+          <h4>Disclaimer</h4>
+          <p>
+            Please note that the company featured on this website is fictitious
+            and has been created solely for the purpose
+            <br /> of the "Hack the Globe" hackathon organized by BCG. All
+            content, including text, images, and multimedia, <br />
+            is provided for simulation purposes only and does not pertain to any
+            real entity or organization.
+          </p>
+        </div>
+      </div>
+      <div className="footer-bottom">
+        &copy; {new Date().getFullYear()} Your Company Name | All Rights
+        Reserved
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
--- frg/src/layouts/HomePage.jsx
+++ frg/src/layouts/HomePage.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import cover from "../assets/cover-photo.png";
 import "./HomePage.css";
+import Footer from "../components/Footer";
 function HomePage() {
   return (
     <div className="home-page">
@@ -29,6 +30,8 @@ function HomePage() {
         <p>Our Top Sponsors</p>
         <div className="logos"></div>
       </section>
+
+      <Footer />
     </div>
   );
 }
