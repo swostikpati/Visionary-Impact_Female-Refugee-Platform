908b37b9a4963a6e2cea1d094886a260091429ff
swostikpati <swostikpati@gmail.com>
2024-03-16 17:01:34 +0000

    added stories section

--- frg/src/layouts/HomePage.jsx
+++ frg/src/layouts/HomePage.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import cover from "../assets/cover-photo.png";
+import refugee1 from "../assets/refugee1.png";
+import refugee2 from "../assets/refugee2.png";
+import refugee3 from "../assets/refugee3.png";
 import "./HomePage.css";
 function HomePage() {
   return (
@@ -21,6 +24,24 @@ function HomePage() {
           <div className="num-elems sponsors">
             <p className="num">60+</p>
             <p className="num-text">Sponsors</p>
+          </div>
+        </div>
+      </section>
+
+      <section className="stories">
+        <p>Our Success Stories</p>
+        <div className="story-cards">
+          <div className="story-card">
+            <img src={refugee1} alt="Cover Photo" />
+            <p>"In unity, our differences shine."</p>
+          </div>
+          <div className="story-card">
+            <img src={refugee2} alt="Cover Photo" />
+            <p>"Independence blooms within me."</p>
+          </div>
+          <div className="story-card">
+            <img src={refugee3} alt="Cover Photo" />
+            <p>"Laughter, our universal language."</p>
           </div>
         </div>
       </section>
